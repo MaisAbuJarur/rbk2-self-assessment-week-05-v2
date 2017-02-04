9bbdb4a406557807c4c455d522f4e09d04d60a4d
MaisAbuJarur <maisabujarur@gmail.com>
2017-02-04 10:03:25 +0200

    four

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -18,33 +18,40 @@ angular.module('app', ['ngRoute'])
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function($scope){
-   $scope = {};
-    $scope.counter.value = 0
+  .factory('counter', function(){
+   var counter = {};
+    counter.value = 0
+    
 
 
   })
-  .controller('fizzbuzzCtrl', function($scope){
-    if($scope.counter.value % 3===0){
-      $scope.display = "FIZZ"
+  .controller('fizzbuzzCtrl', function(counter){
+    if(counter.value % 3===0){
+      counter.display = "FIZZ"
+
     }
-    if($scope.counter.value% 5===0){
-      $scope.display = "BUZZ"
+    if(counter.value% 5===0){
+      counter.display = "BUZZ"
     }
-    if($scope.counter.value % 3===0 && $scope.counter.value % 5===0 ){
-      $scope.display = "FIZZBUZZ"
+    if(counter.value % 3===0 && $scope.counter.value % 5===0 ){
+      counter.display = "FIZZBUZZ"
     }
 
   })
-  .controller('fozzbazzCtrl', function(){});
- if($scope.counter.value% 4===0){
-      $scope.display = "FOZZ"
-    }
-    if($scope.counter.value% 6===0){
-      $scope.display = "BAZZ"
-    }
-    if($scope.counter.value % 3===0 && $scope.counter.value % 5===0 ){
-      $scope.display = "FOZZBAZZ"
-    }
+  .controller('fozzbazzCtrl', function(counter){
+   if(counter.value% 4===0){
+    display = "FOZZ"
+    
+  }
+  if(counter.value% 6===0){
+    display = "BAZZ"
+  }
+  if(counter.value % 3===0 && counter.value % 5===0 ){
+    display = "FOZZBAZZ"
+  }
 
-  })
+})
+
+  var increment = function ($scope){
+    counter.value += 1
+  }
