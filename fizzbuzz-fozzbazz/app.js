33499f096078b0002cddb6162270e1e3c430f3cb
MaisAbuJarur <maisabujarur@gmail.com>
2017-02-04 08:56:37 +0200

    one

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -18,6 +18,16 @@ angular.module('app', ['ngRoute'])
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function(){})
-  .controller('fizzbuzzCtrl', function(){})
+  .factory('counter', function($scope){
+    $scope.counter = 0;
+
+
+  })
+  .controller('fizzbuzzCtrl', function($scope){
+    if($scope.counter% 3===0){
+      
+    }
+
+
+  })
   .controller('fozzbazzCtrl', function(){});
